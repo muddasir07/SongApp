383bce7e5c16f0c43c5d15129e7dbc736503c804
Muddasir Shabbir <muddasirshabbir59@gmail.com>
2023-05-04 09:16:25 -0700

    Music Web App
    Music Player

---
+++ js/playlist.js
@@ -0,0 +1,26 @@
+let songs = [
+  {
+    name: "Let Me Down Slowly",
+    artist: "Alec Benjamin",
+    img: "img1",
+    audio: "music1"
+  },
+  {
+    name: "Arcade",
+    artist: "Duncan Laurence ft. FLETCHER",
+    img: "img2",
+    audio: "music2"
+  },
+  {
+    name: "In The End",
+    artist: "Linkin Park",
+    img: "img3",
+    audio: "music3"
+  },
+  {
+    name: "Senorita - Shawn Mendes",
+    artist: "Shawn Mendes, Camila Cabello",
+    img: "img4",
+    audio: "music4"
+  }
+]
